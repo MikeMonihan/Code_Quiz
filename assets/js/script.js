d4619c7cfcf7559fb32cc6b7b29e47d39fd8c820
Michael Monihan <mrmonihan@hotmail.com>
2021-12-19 20:36:54 -0500

    Added timer and finished basic quiz structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,40 @@
 const ansOne = document.getElementById("ansOne");
 
-
-function startTimer(length, display){
-    setInterval(function (){
-        display.textContent = "Timer: " + length;
-    });
-
-}
-
-
-
 document.getElementById("btnSubmit").addEventListener("click", function(){
     var timerDefault = 75;
-    display = document.getElementById("quizTimer");
-    startTimer(timerDefault, display);
-    
+    var countdownTimer = setInterval(function(){
+        timerDefault--;
+        if(timerDefault <= 0){
+            clearInterval(countdownTimer);
+            document.getElementById("quizTimer").innerHTML = "Timer: 0";
+            document.getElementById("ansOne").className ="hideMe";
+            document.getElementById("ansTwo").className ="hideMe";
+            document.getElementById("ansThree").className ="hideMe";
+            document.getElementById("ansFour").className ="hideMe";
+
+            document.getElementById("flavText").className ="seeMe";
+
+            //Add in score
+            document.getElementById("flavText").innerHTML = "Your final score is: ";
+
+            document.getElementById("initialsText").className ="seeMe";
+
+            document.getElementById("userName").className ="seeMe";
+
+            document.getElementById("submitBtn").className ="seeMe";
+
+            document.getElementById("quizText").innerHTML = "All Done!";
+
+        } else{
+            document.getElementById("quizTimer").innerHTML = "Timer: " + timerDefault;
+        }
+    },1000)
+    document.getElementById("ansOne").className ="seeMe";
+    document.getElementById("ansTwo").className ="seeMe";
+    document.getElementById("ansThree").className ="seeMe";
+    document.getElementById("ansFour").className ="seeMe";
+    document.getElementById("btnSubmit").className ="hideMe";
+    document.getElementById("flavText").className ="hideMe";
+
+
 })
